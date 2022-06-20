3a07155619a4526e7e881bd9a0510236f3c2071f
lindazhao678 <lindazhao678@gmail.com>
2022-06-20 11:06:30 +1000

    server added

--- index.js
+++ index.js
@@ -2,10 +2,8 @@ const express = require('express')
 const cors = require('cors')
 const axios = require('axios')
 require('dotenv').config()
-const config= require('config');
+const config = require('config');
 
-const pxApiRootURL = config.get("pxApiRootURL");
-const pxApiKey= config.get("pxApiKey");
 const perPage = config.get("perPage");
 const imageType = config.get("imageType");
 
@@ -14,18 +12,22 @@ const app = express()
 app.use(cors())
 
 app.get('/', (req, res) => {
-    const api = `${pxApiRootURL}/?key=${pxApiKey}&per_page=${perPage}&image_type=${imageType}`;
+    const q = req.query.q;
+    const page = req.query.page;
+
+    const api = `${process.env.PX_API_ROOTURL}/?key=${process.env.PX_API_KEY}&q=${q}&page=${page}&per_page=${perPage}&image_type=${imageType}`;
+
     axios.get(api)
-    .then((response) => {
-        res.json(response.data)
-    })
-    .catch((err) => {
-        console.log(err)
-    })
+        .then((response) => {
+            res.json(response.data)
+        })
+        .catch((err) => {
+            console.log(err)
+        })
 })
 
 let port = process.env.PORT;
 if (port == null || port == "") {
-  port = 5000;
+    port = 8000;
 }
 app.listen(port, () => console.log(`Server is running on ${port}`))
